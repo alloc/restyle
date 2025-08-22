446fbd5a792072950ee3d2f0875165a882107002
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-08-21 22:50:27 -0400

    fix: spacing props don’t support string values

--- src/restyleFunctions.ts
+++ src/restyleFunctions.ts
@@ -323,14 +323,14 @@ export interface BackgroundColorShorthandProps<Theme extends BaseTheme> {
 
 export type SpacingProps<Theme extends BaseTheme> = {
   [Key in keyof typeof spacingProperties]?: ResponsiveValue<
-    ThemeKey<Theme, 'spacing'> | number | (string & {}),
+    ThemeKey<Theme, 'spacing'> | number,
     Theme['breakpoints']
   >;
 };
 
 export type SpacingShorthandProps<Theme extends BaseTheme> = {
   [Key in keyof typeof spacingPropertiesShorthand]?: ResponsiveValue<
-    ThemeKey<Theme, 'spacing'> | number | (string & {}),
+    ThemeKey<Theme, 'spacing'> | number,
     Theme['breakpoints']
   >;
 };
