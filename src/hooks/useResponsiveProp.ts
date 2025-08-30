404206a932a400f3278d11c78ce7a5028805a9d7
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-08-29 21:09:12 -0400

    feat: add `themeKey` parameter to useResponsiveProp

--- src/hooks/useResponsiveProp.ts
+++ src/hooks/useResponsiveProp.ts
@@ -1,26 +1,33 @@
 import {useWindowDimensions} from 'react-native';
 
 import {ResponsiveBaseTheme, ResponsiveValue} from '../types';
-import {getValueForScreenSize, isResponsiveObjectValue} from '../utilities';
+import {
+  getThemeValue,
+  getValueForScreenSize,
+  isResponsiveObjectValue,
+} from '../utilities';
 
 import useTheme from './useTheme';
 
-const useResponsiveProp = <
-  Theme extends ResponsiveBaseTheme,
-  TVal,
->(
+const useResponsiveProp = <Theme extends ResponsiveBaseTheme, TVal>(
   propValue: ResponsiveValue<TVal, Theme['breakpoints']>,
+  themeKey?: keyof Theme,
 ) => {
   const theme = useTheme<Theme>();
   const dimensions = useWindowDimensions();
 
-  return isResponsiveObjectValue(propValue, theme)
-    ? getValueForScreenSize({
-        responsiveValue: propValue,
-        breakpoints: theme.breakpoints,
-        dimensions,
-      })
-    : propValue;
+  let responsiveValue: any = propValue;
+  if (isResponsiveObjectValue(propValue, theme)) {
+    responsiveValue = getValueForScreenSize({
+      responsiveValue,
+      breakpoints: theme.breakpoints,
+      dimensions,
+    });
+  }
+
+  return themeKey
+    ? getThemeValue(responsiveValue, {theme, themeKey})
+    : responsiveValue;
 };
 
 export default useResponsiveProp;
